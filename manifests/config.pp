b6149d073677fbe1649edb91ea77a74782b81b50
beergeek <brett@puppet.com>
2018-08-04 16:14:20 +0800

    testing

--- manifests/config.pp
+++ manifests/config.pp
@@ -102,7 +102,7 @@ class jira::config {
       line    => "    <property name=\"hibernate.connection.driver_class\">${_db_driver}</property>",
       match   => '^( |\t)*<property name\="hibernate.connection.driver_class">',
       after   => '^( |\t)*<property name\="jira.jms.broker.uri">',
-      require => File['base_config'],
+      #require => File['base_config'],
     }
 
     file_line { 'db_password':
